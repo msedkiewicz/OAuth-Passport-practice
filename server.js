204615ba9bcb65084727b43a960587d0b1523eba
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-12 23:52:30 +0100

    Fix configuration

--- server.js
+++ server.js
@@ -1,49 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const hbs = require('express-handlebars');
-const passport = require('passport');
-const session = require('express-session');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const hbs = require("express-handlebars");
+
+const passport = require("passport");
+const passportConfig = require("./config/passport");
+const session = require("express-session");
 
 const app = express();
 
-app.use(session({ secret: 'anything' }));
+// session config
+app.use(session({ secret: "anything" }));
+
+// passport config
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/login/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-  app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
+// app handlebars
+app.engine(
+  "hbs",
+  hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
 );
+app.set("view engine", ".hbs");
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
-app.set('view engine', '.hbs');
-
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/user", require("./routes/user.routes"));
+
+app.use("/", (req, res) => {
+  res.status(404).render("notFound");
 });
 
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
-
-app.use('/', (req, res) => {
-  res.status(404).render('notFound');
-});
-
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+app.listen("8000", () => {
+  console.log("Server is running on port: 8000");
 });
